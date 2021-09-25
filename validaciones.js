47ec8cd05d9c3d4b6b7df04f64e14cda4e7a1ed4
paocaos7 <paolacastaneda1997@gmail.com>
2021-09-25 17:56:05 -0500

    Union funcion correo a registro html

--- validaciones.js
+++
@@ -1,8 +0,0 @@
-function validar_correo(string) {
-    let rgex = /(\w{1,})@(\w{1,})\.(com|es|co|org)$/i;
-    if (string.length > 14 && string.length < 40 && rgex.test(string)) {
-        return true;
-    }
-    return false;
-}
-module.exports.validar_correo = validar_correo
